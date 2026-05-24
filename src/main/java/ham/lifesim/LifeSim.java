afb8048bbf8e82e34b07bc7c916693b7c070df70
2048Israel <Jerusalem2048@outlook.com>
2026-05-24 16:41:37 -0300

    Initial commit: Set up Maven project with JavaFX

---
+++ src/main/java/ham/lifesim/LifeSim.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ham.lifesim;
+
+/**
+ *
+ * @author Jerus
+ */
+public class LifeSim {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
